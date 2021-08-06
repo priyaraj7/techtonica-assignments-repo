1ca9e877141516ce95498898394ccdd8db23ea91
Priyaraj7 <suppichala@gmail.com>
2021-08-05 19:40:40 -0700

    add index script css files

---
+++ script.js
@@ -0,0 +1,22 @@
+// get random number between 0 and 1
+
+function getRandomInt() {
+  return Math.floor(Math.random() * 2);
+}
+
+let button = document.getElementById("flip");
+let result = document.getElementById("result");
+
+button.addEventListener("click", function () {
+  let num = getRandomInt();
+
+  if (num === 0) {
+    //alert("Its head");
+    result.innerHTML = "Yay! You got HEAD";
+    result.style.backgroundColor = "Turquoise";
+  } else {
+    //alert("Its tail");
+    result.innerHTML = "Oops! You got Tail";
+    result.style.backgroundColor = "Tomato";
+  }
+});
