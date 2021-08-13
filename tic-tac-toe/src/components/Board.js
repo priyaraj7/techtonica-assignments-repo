ab081bd2893178afe6632814046983b7c264ccdc
Priyaraj7 <suppichala@gmail.com>
2021-08-13 01:13:56 -0700

    add working project

--- tic-tac-toe/src/components/Board.js
+++ tic-tac-toe/src/components/Board.js
@@ -1,54 +1,12 @@
 import React from "react";
 import Square from "./Square";
 
-const Board = ({ squares, onClick }) => {
+const Board = ({ squares = [], onClick = () => {} }) => {
   return (
     <div className="board">
-      <Square
-        className="boardSquare"
-        value="1"
-        onClick={() => onClick("dummy")}
-      />
-      <Square
-        className="boardSuare"
-        value="2"
-        onClick={() => onClick("dummy")}
-      />
-      <Square
-        className="boardSuare"
-        value="3"
-        onClick={() => onClick("dummy")}
-      />
-      <Square
-        className="boardSuare"
-        value="4"
-        onClick={() => onClick("dummy")}
-      />
-      <Square
-        className="boardSuare"
-        value="5"
-        onClick={() => onClick("dummy")}
-      />
-      <Square
-        className="boardSuare"
-        value="6"
-        onClick={() => onClick("dummy")}
-      />
-      <Square
-        className="boardSuare"
-        value="7"
-        onClick={() => onClick("dummy")}
-      />
-      <Square
-        className="boardSuare"
-        value="8"
-        onClick={() => onClick("dummy")}
-      />
-      <Square
-        className="boardSuare"
-        value="9"
-        onClick={() => onClick("dummy")}
-      />
+      {squares.map((square, i) => {
+        return <Square key={i} value={square} onClick={() => onClick(i)} />;
+      })}
     </div>
   );
 };
--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -1,11 +1,41 @@
-import React from "react";
+//import React from "react";
+import React, { useState } from "react";
+import { calculateWinner } from "../winningfunction";
 import Board from "./Board";
 
 const Game = () => {
-  const handleClick = () => {};
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(board);
 
-  const renderMoves = () => {};
-  return <Board onClick={handleClick} />;
+  const handleClick = (i) => {
+    const boardCopy = [...board];
+    // if user click an occupied square of if game is won, return
+    if (winner || boardCopy[i]) return;
+    // put an X or O in the clicked square
+    boardCopy[i] = xIsNext ? "X" : "O";
+    setBoard(boardCopy);
+    setXisNext(!xIsNext);
+  };
+
+  const renderMoves = () => (
+    <button onClick={() => setBoard(Array(9).fill(null))}>Start Game</button>
+  );
+
+  return (
+    <>
+      <Board squares={board} onClick={handleClick} />;
+      <div className="winner">
+        <p>
+          {winner
+            ? "Winner: " + winner
+            : "Next Player: " + (xIsNext ? "X" : "O")}
+        </p>
+
+        {renderMoves()}
+      </div>
+    </>
+  );
 };
 
 export default Game;
