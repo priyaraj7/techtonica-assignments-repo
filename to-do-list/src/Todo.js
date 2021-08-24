03f80ab35096bf0663c9bdbb45888d98f64bfac0
Priyaraj7 <suppichala@gmail.com>
2021-08-23 23:08:58 -0700

    add minor changes

--- to-do-list/src/Todo.js
+++ to-do-list/src/Todo.js
@@ -1,15 +1,45 @@
 import React, { useState } from "react";
 
-function Task({ task }) {
+function Task({ task, index, completeTask, removeTask }) {
   return (
     <div
       className="task"
       style={{ textDecoration: task.completed ? "line-through" : "" }}
     >
       {task.title}
+      <button style={{ background: "red" }} onClick={() => removeTask(index)}>
+        x
+      </button>
+      <button onClick={() => completeTask(index)}>Complete</button>
     </div>
   );
 }
+// adding new task
+function CreateTask({ addTask }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+
+    addTask(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add a new task"
+        onChange={(e) => setValue(e.target.value)}
+      ></input>
+    </form>
+  );
+}
+
+//todo component
 
 function Todo() {
   const [tasks, setTasks] = useState([
@@ -22,13 +52,38 @@ function Todo() {
       compleated: true,
     },
   ]);
+
+  const addTask = (title) => {
+    const newTasks = [...tasks, { title, completed: false }];
+    setTasks(newTasks);
+  };
+  const completeTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = true;
+    setTasks(newTasks);
+  };
+  const removeTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
   return (
     <div className="todo-container">
       <div className="header">TODO - ITEMS</div>
-      <div className="task">
+      <div className="tasks">
         {tasks.map((task, index) => (
-          <Task task={task} index={index} key={index} />
+          <Task
+            task={task}
+            index={index}
+            completeTask={completeTask}
+            removeTask={removeTask}
+            key={index}
+          />
         ))}
+      </div>
+      <div className="create-task">
+        <CreateTask addTask={addTask} />
       </div>
     </div>
   );
