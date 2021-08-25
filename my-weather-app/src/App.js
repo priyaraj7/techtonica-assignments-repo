c10f410b3028dca3387ba70cd4996d529f341e25
Priyaraj7 <suppichala@gmail.com>
2021-08-25 13:15:40 -0700

    create components

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -6,8 +6,12 @@ function App() {
       <header className="App-header">
         <h1>Hello</h1>
       </header>
+      <main>{/* fetching component */}</main>
+      <footer>@ Supriya</footer>
     </div>
   );
 }
 
 export default App;
+
+//https://rapidapi.com/blog/weather-app-react/
---
+++ my-weather-app/src/components/Forecast/Forecast.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Forecast = () => {
+
+    function getForecast(){
+         // weather data fetch function will go here
+        
+    }
+
+    return ( 
+        // JSX code will go here
+     );
+}
+ 
+export default Forecast;
